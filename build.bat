2ed14cfef76ca01ebe2e22134952c403c68c833b
Stuart Mathews <stumathews@gmail.com>
2025-06-19 12:21:48 +0200

    make dedicated build directory for cmake

--- build.bat
+++
@@ -1,3 +0,0 @@
-if not exist c:\repos\libmonad\CMakeCache.txt cmake -DCMAKE_TOOLCHAIN_FILE=c:\repos\vcpkg\scripts\buildsystems\vcpkg.cmake .
-
-cmake --build .
---
+++ build/build.bat
@@ -0,0 +1,3 @@
+if not exist c:\repos\libmonad\CMakeCache.txt cmake -DCMAKE_TOOLCHAIN_FILE=c:\repos\vcpkg\scripts\buildsystems\vcpkg.cmake ..
+
+cmake --build .
